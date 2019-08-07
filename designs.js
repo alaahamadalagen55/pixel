f68fa7db83b4c3d8c10263205c456b702a6c31f7
alaahamadalagen55 <53879211+alaahamadalagen55@users.noreply.github.com>
2019-08-07 22:34:44 +0200

    Create designs.js

---
+++ designs.js
@@ -0,0 +1,25 @@
+document.addEventListener("submit",table);
+
+function table(event){
+	event.preventDefault();
+	
+	var hight=document.getElementById("inputHeight").value;
+	var width=document.getElementById("inputWidth").value;
+	var shape=document.getElementById("pixelCanvas");
+	shape.innerHTML="";
+	for(var x = 0; x < hight ; x++){
+		     var row=document.createElement("tr");
+		for(var y = 0;y < width ; y++){
+			 var cell=document.createElement("td");
+			 cell.addEventListener("click",farben);
+			 row.appendChild(cell);
+			
+		}
+		shape.appendChild(row);
+	}
+}
+
+function farben(event){
+	var color=document.getElementById("colorPicker").value;
+	event.target.bgColor=color;
+}
